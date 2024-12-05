67a8f4ad393638f36638346407311d6b7a14d403
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-05 20:18:43 -0300

    Finalizado!

---
+++ Serde e Serde JSON/ex04.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize)]
+struct Person{
+    #[serde(rename = "firstName")]
+    first_name: String,
+
+    #[serde(rename = "lastName")]
+    last_name: String,
+    
+    age: u32
+}
+
+fn main(){
+    let p1 = Person{
+        first_name: "Rust".to_string(),
+        last_name: "lang".to_string(),
+        age: 11
+    };
+
+    let serialized = serde_json::to_string(&p1).unwrap();
+    println!("{}", serialized);
+}
